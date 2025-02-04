c75ac920c700744b5e7d73edd202ec9fae4878a4
Yuvaraj.p <saralapalani185@gmail.com>
2025-02-04 15:44:31 +0530

    day2 code

---
+++ day2/CalculateDays.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CalculateDays {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int days=0;
+        System.out.println("Enter the month");
+        int months=sc.nextInt();
+        switch(month) {
+
+        }
+
+    }
+}
