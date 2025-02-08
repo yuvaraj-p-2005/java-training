699d0d29a23b3ff61031b42cb72de95b7d6f4e2b
Yuvaraj.p <saralapalani185@gmail.com>
2025-02-08 15:39:05 +0530

    added

--- day6/StringDemo1.java
+++ day6/StringDemo1.java
@@ -1,4 +1,4 @@
-public class StringDemo {
+public class StringDemo1 {
     public static void main(String[] args) {
         String str="hello world";
 
---
+++ day6/StringDemo2.java
@@ -0,0 +1,15 @@
+public class StringDemo2 {
+    public static void main(String[] args) {
+        String str="Hello world";
+        str=str.toLowerCase();//hello world
+        
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.CharAt(i)=='a'||)
+        }
+
+            
+        
+    }
+    
+}
